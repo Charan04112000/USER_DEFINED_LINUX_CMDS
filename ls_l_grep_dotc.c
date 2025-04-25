4ccc3cd8feb38df68803bd86429732e4d56c3544
Etikala Sai Charan Reddy <etikala441@gmail.com>
2025-04-25 10:23:55 +0530

    Add files via upload

---
+++ ls_l_grep_dotc.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <error.h>
+//#include <wait/sys.h>
+#include <sys/wait.h>
+int main()
+{
+        int fd1[2],fd2[2];
+        pid_t pid1,pid2;
+
+        if(pipe(fd1)==-1)
+        {
+                perror("Perror");
+                exit(0);
+        }
+
+        if((pid1=fork())==0)
+        {
+                dup2(fd1[1],STDOUT_FILENO);
+                close(fd1[0]);
+                close(fd1[1]);
+
+                execlp("ls","ls","-l",NULL);
+                perror("execlp");
+                exit(1);
+        }
+
+        else if((pid2=fork())==0)
+        {
+                dup2(fd1[0],STDIN_FILENO);
+                close(fd1[1]);
+                close(fd1[0]);
+
+                execlp("grep","grep",".c",NULL);
+                perror("execlp");
+                exit(1);
+        }
+
+        close(fd1[1]);
+        close(fd1[0]);
+
+        wait(NULL);
+        wait(NULL);
+        return 0;
+}
